9442831e7f919e2c2ad41430ecf046230597a4a9
Luchienlanh <lucdo2511@gmail.com>
2026-08-23 09:47:04 +0700

    knfst

--- limnfst/competitors.py
+++ limnfst/competitors.py
@@ -66,11 +66,13 @@ def learn_knfst(kernel_matrix, y):
         _, fallback_vectors = np.linalg.eigh(within_scatter)
         eigenvectors = fallback_vectors[:, :1]
 
-    projection = centered_basis @ eigenvectors
+    projection = np.asarray(centered_basis @ eigenvectors).real
     centroids = []
     for class_label in classes:
         class_kernel = kernel_matrix[:, y == class_label]
-        centroids.append(np.mean(class_kernel.T @ projection, axis=0))
+        centroids.append(
+            np.asarray(class_kernel.T @ projection).mean(axis=0).ravel()
+        )
     return projection, np.asarray(centroids).real
 
 
